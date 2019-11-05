0f483174c6a9d4655990da5da79bfd57c3132fc5
Taylor Cool <taycool@gmail.com>
2019-11-05 13:16:22 -0800

    updated data, numbers, operators, specials

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, {useState} from "react";
+import {numbers} from "../../../data"
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 /* 
@@ -7,13 +7,18 @@ import { numbers } from '../../../data'
 */
 //Import your array data to from the provided data file
 
-const Numbers = () => {
+
+export const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberButton, setNumberButton] = useState(numbers)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      it any props needed by the child component*/}
+       {data.map((arr) => {
+         <button className='button'>{arr.numbers}</button>
+       })}
     </div>
   );
 };
